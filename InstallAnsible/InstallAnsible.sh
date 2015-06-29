7d10e3eef8748890fad2c61fe1672c4e69c941a7
Dmitry Karanfilov <kdsnice@gmail.com>
2015-06-30 00:23:13 +0300

    Small fix

--- InstallAnsible/InstallAnsible.sh
+++ InstallAnsible/InstallAnsible.sh
@@ -11,7 +11,7 @@ echo "Cloning Ansible"
 git clone git://github.com/ansible/ansible.git --recursive ansible
 cd ansible
 git checkout stable-1.9
-echo Updating profile
+echo "Updating profile"
 echo source /opt/ansible/hacking/env-setup >> ~/.profile
 echo export ANSIBLE_INVENTORY=~/ansible_hosts >> ~/.profile
 echo export ANSIBLE_HOST_KEY_CHECKING=False >> ~/.profile
